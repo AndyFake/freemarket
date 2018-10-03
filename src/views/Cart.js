9fa364d9c5a9f35ac96c13f45803829a739be79e
Andy March <andymarch@A-Marchs-MBP.gv.shawcable.net>
2018-10-02 18:46:36 -0700

    clean_up_css

--- src/views/Cart.js
+++ src/views/Cart.js
@@ -8,55 +8,51 @@ import {PlusSquare, MinusSquare, XSquare} from 'react-feather'
 const Cart = () =>
   <div className='Cart-Container'>
     <Link to='/store'><div className='Cart-Back'>continue shopping</div></Link>
-    <div className='Items-Container'>
-      {State.cart.length<=0 && 
-        <div className='Cart-Empty-Message'>
-          Cart is Empty
+    {State.cart.length<=0 && 
+      <div className='Cart-Empty-Message'>
+        Cart is Empty
+      </div>
+    }
+    {State.cart.map((item,i) => 
+      <div className='Cart-Line'>
+        <div 
+          className='Cart-Remove' 
+          onClick={()=>{State.RFC(i)}}
+        >
+          <XSquare className='Cart-Feather'/>
         </div>
-      }
-      {State.cart.map((item,i) => 
-        <div className='Cart-Line'>
+        <div className='Cart-Item-Name'>
+          <div className='Cart-Item-Name-Text'>{item.title}</div>
+          {item.options.length>0 && 
+            <div className='Cart-Item-Name-Text' style={{color:'grey'}}>
+              {item.options}
+            </div>
+          }
+        </div>
+        <div className='Cart-Quantity-Widget'>
           <div 
-            className='Cart-Remove' 
-            onClick={()=>{State.RFC(i)}}
+            onClick={()=>{State.modCart(i,item.quantity+1)}}
           >
-            <XSquare className='Cart-Feather'/>
+          <PlusSquare className='Cart-Feather'/>
           </div>
-          <div className='Cart-Item-Name'>
-            <div>{item.title}</div>
-            {item.options.length>0 && 
-              <div style={{color:'grey'}}>
-                {item.options}
-              </div>
-            }
+          <div className='Cart-Item-Quantity'>
+            <input
+              ref={el=>this[i]=el}
+              style={{width:`${String(item.quantity).length*8+5}px`,minWidth:'23px', textAlign:'center',height:'25px',margin:'3px',padding:'0px'}}
+              value={item.quantity?item.quantity:''}
+              onChange={e=>State.modCart(i,parseInt(e.target.value)||0)}
+              onKeyPress={e=>e.key==='Enter'&&this[i].blur()}
+            />
           </div>
-          <div className='Cart-Item-Price'>${item.price*item.quantity}</div>
-          <div className='Cart-Quantity-Widget'>
-            <div 
-              className='Cart-Button' 
-              onClick={()=>{State.modCart(i,item.quantity+1)}}
-            >
-            <PlusSquare className='Cart-Feather'/>
-            </div>
-            <div className='Cart-Item-Quantity'>
-              <input
-                ref={el=>this[i]=el}
-                style={{textAlign:'center',position:'relative',height:'25px',width:'30px',margin:'3px',padding:'0px'}}
-                value={item.quantity?item.quantity:''}
-                onChange={e=>State.modCart(i,parseInt(e.target.value)||0)}
-                onKeyPress={e=>e.key==='Enter'&&this[i].blur()}
-              />
-            </div>
-            <div 
-              className='Cart-Button'
-              onClick={()=>{item.quantity>1&&State.modCart(i,item.quantity-1)}}
-            >
-              <MinusSquare className='Cart-Feather'/>
-            </div>
+          <div 
+            onClick={()=>{item.quantity>1&&State.modCart(i,item.quantity-1)}}
+          >
+            <MinusSquare className='Cart-Feather'/>
           </div>
         </div>
-      )}
-    </div>
+        <div className='Cart-Item-Price'>${item.price*item.quantity}</div>
+      </div>
+    )} 
     <div className='Cart-Footer'>
       <div className='Cart-Footer-Total'>
         TOTAL : ${State.getTotal()}
