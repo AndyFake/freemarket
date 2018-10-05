1dba13389c2eebff9a349d22b271674307dca674
Andy March <andymarch@A-Marchs-MBP.gv.shawcable.net>
2018-10-05 12:00:08 -0700

    add_stock_Checkout

--- src/widgets/TestWidget.js
+++ src/widgets/TestWidget.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 // import styled from "styled-components";
 
 // This is the editing component
-export class SlidesControl extends Component {
+export class TestWidgetControl extends Component {
   render() {
     return <div>testwidget</div>;
   }
 }
 
 // This is the preview component
-export const SlidesPreview = props => <div></div>;
+export const TestWidgetPreview = props => <div></div>;
