bf716b1b155b74b7abe518881035af4573e9c426
Andy March <youthclubrecords@gmail.com>
2018-10-06 18:43:50 -0700

    add_stock

--- src/widgets/TestWidget.js
+++ src/widgets/TestWidget.js
@@ -1,29 +1,50 @@
 import React, { Component } from "react";
-import uuid from 'uuid/v4';
+
+// import uuid from 'uuid/v4';
 // import styled from "styled-components";
 
 // This is the editing component
 export class TestWidgetControl extends Component {
   constructor(){
-    super(props)
-    this.controlID=uuid()
+    super()
+    this.state={data:'not done'}
   }
-  componentDidMount() {
-    const collection = 'products';
-    const searchFields = 'status';
-    const value = 'published'
-    this.props.query(this.controlID, collection, searchFields, value);
+  componentDidMount(){
+    fetch( URL, { method:"GET" } )
+    .then(r => r.json() )
+    .then(r =>{
+      console.log('fetched')
+      this.setState({data:done})
+  })
+    // .then(r => console.log(atob(r.content)) )
   }
-  render() {
-    const {queryHits} = this.props
-    console.log(JSON.stringify(queryHits))
-    return (
-      <div>
-        {queryHits.get(this.controlID).map(hit=><div>{hit.data.title}</div>)}
-      </div>
+  render(){
+    return(
+      <div>{this.state.data}</div>
     )
   }
 }
+
+//   constructor(){
+//     super(props)
+//     this.controlID=uuid()
+//   }
+//   componentDidMount() {
+//     const collection = 'products';
+//     const searchFields = 'status';
+//     const value = 'published'
+//     this.props.query(this.controlID, collection, searchFields, value);
+//   }
+//   render() {
+//     const {queryHits} = this.props
+//     console.log(JSON.stringify(queryHits))
+//     return (
+//       <div>
+//         {queryHits.get(this.controlID).map(hit=><div>{hit.data.title}</div>)}
+//       </div>
+//     )
+//   }
+// }
 
 // This is the preview component
 export const TestWidgetPreview = props => <div></div>;
