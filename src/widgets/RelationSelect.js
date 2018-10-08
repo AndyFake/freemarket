07f9f0e8aa729f644dbef6630538bd670f1eaffa
Andy March <youthclubrecords@gmail.com>
2018-10-08 15:14:58 -0700

    make_store_json

--- src/widgets/RelationSelect.js
+++ src/widgets/RelationSelect.js
@@ -1,15 +1,40 @@
 import React from 'react'
 
+const URL = `https://api.github.com/repos/marchingband/freemarket/contents/content/store/store.json`
+
 export function RelationSelectControl(SelectWidget){
   class RelationSelectControl extends React.Component{
     constructor(props){
       super(props)
       console.log(JSON.stringify(props))
+      this.state={
+        loading:'not done',
+        data:[]
+      }
     }
+    // componentDidMount(){
+    //   if(this.state.loading=='not done'){
+    //     fetch( URL, { method:"GET" } )
+    //     .then(r => r.json() )
+    //     .then(r =>{
+    //       var data = []
+    //       var products = JSON.parse(atob(r.content)).products
+    //       for(var p of products){
+    //         stock.push({title:p.title,stock:p.stock})
+    //         for(var {options:o} of p){
+    //           o.separateStock && stock.push({title: p.title + ':' + o.option, stock: o.stock})
+    //         }
+    //       }
+    //       this.setState({data,loading:'done'})
+    //     })
+    //   }
+    // }
     render(){
+      var _props = {...this.props}
+      _props.field.options=['test','test2']
       return(
         <div>
-          <SelectWidget {...this.props} />
+          <SelectWidget {..._props} />
         </div>
 
       )
