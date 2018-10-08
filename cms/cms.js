cb311cbbca3aee1c1008ad99648d808b1152c010
Andy March <youthclubrecords@gmail.com>
2018-10-08 12:51:41 -0700

    make_store_json

--- cms/cms.js
+++ cms/cms.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import {TestWidgetControl,TestWidgetPreview} from '../src/widgets/TestWidget'
-import {MRelationControl, MRelationPreview} from '../src/widgets/MRelation'
+import {TestWidgetControl,TestWidgetPreview} from '../src/widgets/TestWidget.js'
+import {MRelationControl, MRelationPreview} from '../src/widgets/MRelation.js'
 
 
 import '../src/globalStyles.css'
@@ -53,15 +53,15 @@ CMS.registerPreviewTemplate('productStock', ({ entry }) => (
   <ProductPageTemplate fields={entry.toJS().data} />
 ))
 CMS.registerWidget(
-  "test",      // Widget name
+  "stock",      // Widget name
   TestWidgetControl, // Editor component
   TestWidgetPreview  // Preview component (this is optional)
 );
-CMS.registerWidget(
-  "mrelation",      // Widget name
-  MRelationControl, // Editor component
-  MRelationPreview  // Preview component (this is optional)
-);
+// CMS.registerWidget(
+//   "mrelation",      // Widget name
+//   MRelationControl, // Editor component
+//   MRelationPreview  // Preview component (this is optional)
+// );
 
 // Return to home when user logging out
 window.netlifyIdentity.on('logout', function () {
